aa61aa5212c1386d895161dd00aec4f6d98204dd
Vivek Ji Baranwal <vivekjib@gmail.com>
2017-02-02 23:49:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/article-two', function (req, res) {
   res.send('Article two is requested and will be served here');
 });
 
-app.get('/article-th', function (req, res) {
+app.get('/article-three', function (req, res) {
   res.send('Article three is requested and will be served here');
 });
 
